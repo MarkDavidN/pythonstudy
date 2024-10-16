119a7fa3176f9b1f10f7a782c34ff02d84852657
macdavid909 <macdavid909@gmail.com>
2024-10-16 20:41:13 +0300

    initial commit

---
+++ strings.py
@@ -0,0 +1,14 @@
+fname="mark"
+sname="david"
+fulln=fname+" "+ sname
+print(type(fulln))
+print(fulln.upper())
+print(fulln.lower())
+sentence="     mosesmary   "
+print(sentence
+    )
+sentence1=sentence.strip()
+print(sentence1)
+print(len(sentence1))
+myname="msrkdavid"
+print(myname[3])
---
+++ stringtask.py
@@ -0,0 +1,8 @@
+name='   JOHn .'
+name=name.lower()
+print(name)
+name=name.replace('.' '')
+print(name)
+name=name.strip()
+print(name)
+
