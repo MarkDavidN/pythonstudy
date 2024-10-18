89f1b39947a770e91c5924ffe806e83514a96990
MarkDavidN <macdavid909@gmail.com>
2024-10-18 12:27:34 +0300

    update

--- intfloats.py
+++ intfloats.py
@@ -8,3 +8,34 @@ num=int(num)
 num2="2000"
 num2=int(num2)
 print(num+num2)
+
+#Questions
+#Convert a float to an integer with an inbuilt function in Python
+#temp = 56.8926 to 57
+#Convert the float below to give the results as follows
+#temp = 56.8926 to 56.89 
+#Convert the float below to give the results as follows
+#temp = 56.8926 to 56.893 
+#Convert the float below to give the results as follows
+#temp=56.8926 to 8.926 
+#NB: Use string  slice & concatenation, but have result as float 
+
+#1Convert a float to an integer with an inbuilt function in Python
+#temp = 56.8926 to 57
+num1=56.892
+
+
+
+#2Convert the float below to give the results as follows
+#temp = 56.8926 to 56.89 
+num=56.8927
+print(round(num,2))
+
+
+##Convert the float below to give the results as follows
+#temp=56.8926 to 8.926 
+num=56.8927
+
+
+
+
