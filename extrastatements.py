9b15217c352534c51553eaf4f4d179aab480f396
MarkDavidN <macdavid909@gmail.com>
2024-10-28 20:26:34 +0300

    up

--- extrastatements.py
+++ extrastatements.py
@@ -38,3 +38,43 @@ elif start_date>end_date:
 else:
     print ("one day period")   
 
+#2.Given two strings str1 and str2, write a conditional statement that checks:
+#If str1 is longer than str2, print "str1 is longer".
+#If str2 is longer than str1, print "str2 is longer".
+#If both have equal length, print "Both are of equal length".
+
+str1=input("str1 data") 
+str2=input("str2 data")
+if len(str1)>len(str2) :print("str1 is longer")
+elif len(str2)>len(str1) :print("str2 is longer")
+else :print("both are of equal length")
+
+#3.Given a list valid_ids = [101, 102, 103] and a variable user_id = 105, write a conditional statement that:
+#Prints "Access Granted" if user_id is in valid_ids.
+#Prints "Access Denied" if user_id is not in valid_ids.
+valid_ids = [101, 102, 103]
+user_id = 107
+if user_id in(valid_ids) == True :print("access granted")
+else:print("access denied") 
+
+
+#4.Given a variable value that could be of any type, write a conditional statement that:
+#Prints "String Detected" if value is a string.
+#Prints "Integer Detected" if value is an integer.
+#Prints "Unknown Type" for any other type.
+value=input("enter value")
+if (type(value)) ==  str:print("string detected")
+elif (type(value)) == int:print("integer detected")
+else:print('unknown type')
+
+#5.Given x = 7 and y = 14, write nested conditional statements that print:
+#"x and y are both even" if both x and y are even numbers.
+#"Only y is even" if only y is even.
+#"Neither x nor y are even" if both are odd.
+X=7
+y=14
+if X%2==0:
+    if y%2==0:print("x and y are both even")
+    else:print("only x is even")
+elif y%2==0:print("only y is even")
+else:print ("Neither x nor y are even")    
