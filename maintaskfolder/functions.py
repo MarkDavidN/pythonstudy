40e8e63c0837968c7d62b1cfb638750e81c5b8e0
MarkDavidN <macdavid909@gmail.com>
2024-11-05 15:25:22 +0300

    fun

---
+++ maintaskfolder/functions.py
@@ -0,0 +1,15 @@
+#function to calculate area of triangle
+def triangleare():
+    base=int(input("enter base"))
+    height=int(input("enter height"))
+    area=(base*height)/2
+    print(area)
+triangleare()    
+
+#create a function that calculates area of a rectangle
+def recarea():
+    length=int(input("enter length"))
+    width=int(input("width"))
+    area=(width*length)
+    print(area)
+recarea()    
