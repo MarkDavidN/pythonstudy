4bc470131380957077532c720340ab807839ec30
MarkDavidN <macdavid909@gmail.com>
2024-11-05 16:57:17 +0300

    ..

--- maintaskfolder/functions.py
+++ maintaskfolder/functions.py
@@ -11,5 +11,33 @@ def recarea():
     length=int(input("enter length"))
     width=int(input("width"))
     area=(width*length)
-    print(area)
-recarea()    
+    return recarea
+x=recarea()  
+print(recarea) 
+
+def arearec(leng,width):
+    area=leng*width
+    return arearec
+arearec(20,30)  
+
+def evenodd():
+    number=int(input("enter no"))
+    if number%2==0:
+        return "even"
+    else:
+        return "odd"
+evenodd()    
+def evenod(num):
+    if num%2==0:
+        return 'even'
+    else:
+        return 'odd'
+evenod(9)    
+
+#write a program that takes in 4 inputs..print largest and numbers should not be the same
+
+num1=int(input("enter no 1"))
+num2=int(input("enter no 2"))
+num3=int(input("enter no 3"))
+num4=int(input("enter no 4"))
+largest=max(num1,num2,num3,num4)
